94838b2a0544640aca47a0e24802514731307960
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-26 18:38:17 +0530

    Add files via upload

---
+++ ShiftOperators.java
@@ -0,0 +1,12 @@
+public class ShiftOperators {
+    public static void main(String[] args) {
+        //left and right shift operators
+        int a=10;
+        int leftshift=a<<2;
+        System.out.println("left shift of "+a+" is "+leftshift);
+        int rightshift=a>>1;
+        System.out.println("right shift of "+a+" is "+rightshift);
+
+    }
+    
+}
